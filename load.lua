4fedd49301353be85e1779239814c57bb79c2d94
Riley <enriquejoaquingreyes11@gmail.com>
2026-08-25 14:14:19 +0800

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,11 +1,16 @@
-local targetURL = (getgenv().DaKeyless == false)
-	and "https://raw.githubusercontent.com/rileybeerblx1/aurora/refs/heads/main/fahh.lua"
-	or "https://raw.githubusercontent.com/rileybeerblx1/aurora/refs/heads/main/uimobile.lua"
+getgenv().DaKeyless = false
+local targetURL
+
+if getgenv().DaKeyless == false then
+    targetURL = "https://raw.githubusercontent.com/rileybeerblx1/aurora/refs/heads/main/fahh.lua"
+else
+    targetURL = "https://raw.githubusercontent.com/rileybeerblx1/aurora/refs/heads/main/uimobile.lua"
+end
 
 local success, result = pcall(function()
-	return loadstring(game:HttpGet(targetURL))()
+    return loadstring(game:HttpGet(targetURL))()
 end)
 
 if not success then
-	warn("[Aurora Loader] Error loading script: " .. tostring(result))
+    warn("[Aurora Loader] Error loading script: " .. tostring(result))
 end
